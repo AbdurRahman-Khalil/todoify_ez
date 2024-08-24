5050dee1cd331b58a66943ef149105fd2b9aa841
AbdurRahman-Khalil <m.abdurrahmankhalil2@gmail.com>
2024-08-24 15:36:31 +0500

    ...

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ export const Logo = () => {
     return (
         <div className="py-10">
 
-            <h1 className="text-[3rem] font-semibold font-despairs duration-200 ease-linear">
+            <h1 className="text-[3rem] font-semibold font-montserrat duration-200 ease-linear">
                 TodoifyEz
             </h1>
         </div>
